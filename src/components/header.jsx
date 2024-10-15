c08054bfb1587e99c011dc37302f71e2794a0bc2
naimurrahmanniloy <niloyrahman432@gmail.com>
2024-10-15 07:21:23 +0600

    middle point

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,17 +1,61 @@
 import { Link } from "react-router-dom"
 import { Button } from "./ui/button"
+import { SignedIn, SignedOut, SignIn, SignInButton, UserButton } from "@clerk/clerk-react"
+import { PenBox } from "lucide-react"
+import { useState } from "react"
 
 
 const Header = () => {
-    return <>
-        <nav className="py-4 flex justify-between items-center">
-            <Link to="/">
-                <img src="/logo.png" className="h-20" alt="Hirrd Logo" />
-            </Link>
+    const [showSignIn, setShowSignIn] = useState(false);
+    const handleOverlayClick = (e) => {
+        if (e.target === e.currentTarget) {
+            setShowSignIn(false);
 
-            <Button variant="outline">Login</Button>
-        </nav>
-    </>
+        }
+    }
+    return (
+
+        <>
+            < nav className="py-4 flex justify-between items-center" >
+                <Link to="/">
+                    <img src="/logo.png" className="h-20" alt="Hirrd Logo" />
+                </Link>
+
+
+                <div className="flex gap-8">
+                    <SignedOut>
+                        <Button variant="outline" onClick={() => setShowSignIn(true)} >Login</Button>
+                    </SignedOut>
+
+                    <SignedIn>
+
+                        <Button variant="destructive" className="rounded-full">Login</Button>
+                        <PenBox size={20} className="mr-2" />
+                        <Link to={'/post-job'} >
+                        </Link >
+                        <UserButton />
+                    </SignedIn>
+                </div>
+            </nav >
+
+
+            {showSignIn && <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
+                onClick={handleOverlayClick}>
+                <SignIn signUpForceRedirectUrl="/onboarding"
+                    fallbackRedirectUrl="/onboarding"
+                />
+
+            </div>}
+        </>
+
+
+
+
+
+
+    )
+
+
 }
 
 export default Header
--- src/utils/superbase.js
+++ src/utils/superbase.js
@@ -3,6 +3,18 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
 
-export default supabase;
+
+const superbaseClient = async (superbaseAccessToken) => {
+    const superbase = createClient(supabaseUrl, supabaseKey, {
+        global: {
+            headers:
+                { Authorization: `Bearer ${superbaseAccessToken}` }
+        },
+    });
+    return superbase;
+}
+
+
+
+export default superbaseClient;
